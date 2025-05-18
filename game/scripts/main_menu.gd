34bef16e0f60baa4d22905ded8abf56caf5adc5d
Aster Deng <46637392+videogamesrockp@users.noreply.github.com>
2025-05-17 18:53:27 -0700

    add stuff

---
+++ game/scripts/main_menu.gd
@@ -0,0 +1,20 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_start_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://level_1.tscn")
+
+func _on_options_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://options.tscn")
+
+func _on_exit_button_pressed() -> void:
+	get_tree().quit()

8f363558e26297f24fe371c54323509382977cf3
Aster Deng <46637392+videogamesrockp@users.noreply.github.com>
2025-05-17 21:00:18 -0700

    sdf

--- game/scripts/main_menu.gd
+++ game/scripts/main_menu.gd
@@ -11,10 +11,10 @@ func _process(delta: float) -> void:
 	pass
 
 func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://level_1.tscn")
+	get_tree().change_scene_to_file("res://scenes/level_1.tscn")
 
 func _on_options_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://options.tscn")
+	get_tree().change_scene_to_file("res://scenes/options.tscn")
 
 func _on_exit_button_pressed() -> void:
 	get_tree().quit()

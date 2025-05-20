c305943e704f0bcb1b1f40c35ea3a014bb52c95e
Aster Deng <46637392+videogamesrockp@users.noreply.github.com>
2025-05-19 19:40:20 -0700

    fix buttons

--- game/scripts/main_menu.gd
+++ game/scripts/main_menu.gd
@@ -10,11 +10,11 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
-func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/level_1.tscn")
-
-func _on_options_button_pressed() -> void:
+func _on_continue_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/options.tscn")
+
+func _on_new_game_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/level_1.tscn")
 
 func _on_exit_button_pressed() -> void:
 	get_tree().quit()

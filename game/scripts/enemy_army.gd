7ebb4842b485a9434cb414510866e82efd8e6a89
Aarav Shah <53268836+Aarav128@users.noreply.github.com>
2025-05-22 17:30:51 -0700

    ye

--- game/scripts/enemy_army.gd
+++ game/scripts/enemy_army.gd
@@ -3,6 +3,7 @@ extends Node
 
 var villain_scene = preload("res://villain.tscn")
 
+var army = []
 
 func _ready() -> void:
 	spawnArmy(2, 32)
@@ -13,8 +14,10 @@ func spawnArmy(rows, columns) -> void:
 		for j in range(columns):
 			var villain = villain_scene.instantiate()
 			villain.position = Vector2(32 * j, i * 32 + 384);
+			army.append(villain)
 			add_child(villain)
 
 
 func army_movement_timer() -> void:
-	pass # Replace with function body.
+	for c in army:
+		c.position.y -= 32

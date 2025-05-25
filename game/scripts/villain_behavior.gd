af12445a28029ae95c56fc0d630dfaaf0eb2e55a
Aarav Shah <53268836+Aarav128@users.noreply.github.com>
2025-05-25 09:59:54 -0700

    reverted ai pathfinding that didn't work - will add again later

--- game/scripts/villain_behavior.gd
+++ game/scripts/villain_behavior.gd
@@ -13,9 +13,6 @@ var blocked_tile_ids
 var tilemap_layer_node: TileMapLayer = null
 var player_node: CharacterBody2D = null
 
-var pathfinding_grid: AStarGrid2D = AStarGrid2D.new()
-var path_to_player: Array = []
-var turn_counter: int = 1
 
 func _ready() -> void:
 	blocked_tile_ids = self.get_meta("blocked_tile_ids")
@@ -26,33 +23,8 @@ func _ready() -> void:
 	strength = self.get_meta("strength")
 	health = self.get_meta("starting_health")
 	add_to_group("villains")
-	
-	player_node.player_did_a_move.connect(_move_ai)
-	
-	pathfinding_grid.region = tilemap_layer_node.get_used_rect()
-	pathfinding_grid.cell_size = Vector2(TILE_SIZE, TILE_SIZE)
-	pathfinding_grid.diagonal_mode = AStarGrid2D.DIAGONAL_MODE_ONLY_IF_NO_OBSTACLES
-	pathfinding_grid.update()
-	
-	for cell in tilemap_layer_node.get_used_cells():
-		pathfinding_grid.set_point_solid(cell, true)
-		
-	_move_ai()
-	
-func _move_ai():
-	path_to_player = pathfinding_grid.get_point_path(global_position / TILE_SIZE, player_node.global_position / TILE_SIZE)
-	
-	if turn_counter != TURNS_TO_MOVE:
-		turn_counter += 1
-	else:
-		if path_to_player.size() > 1:
-			path_to_player.remove_at(0)
-			var go_to_pos: Vector2 = path_to_player[0] + Vector2(TILE_SIZE/2.0, TILE_SIZE/2.0)
-					
-			global_position = go_to_pos
-		
-		
-			turn_counter = 1
+
+
 
 func _physics_process(_delta: float) -> void:
 	update()
